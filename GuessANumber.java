1c177286758c6d053229db1d648483a3e03341e1
SvetlinStoychev <filipovvenci@yahoo.com>
2023-05-24 12:32:35 +0300

    updated version of the game

--- GuessANumber.java
+++ GuessANumber.java
@@ -7,39 +7,63 @@ public class GuessANumber {
         Scanner reader = new Scanner(System.in);
         Random randomNumber = new Random();
 
-        int computerNumber = randomNumber.nextInt(100);
-
         while (true) {
-            System.out.print("Guess a number (1 - 100): ");
-            String playerInput = reader.nextLine();
 
-            boolean isValidNumber = true;
+            String playerInput = "";
+            int computerNumber = randomNumber.nextInt(100);
+            while (true) {
 
-            for (int i = 0; i < playerInput.length(); i++) {
-                if (playerInput.charAt(i) < 48 || playerInput.charAt(i) > 57) {
-                    isValidNumber = false;
-                }
-            }
+                System.out.print("Guess a number (1 - 100): ");
+                playerInput = reader.nextLine();
 
-            if (isValidNumber) {
-                int playerNumber = Integer.parseInt(playerInput);
+                boolean isValidNumber = true;
 
-                if (playerNumber < 1 || playerNumber > 100) {
-                    System.out.printf("Number [%d] is out of range! Try again!%n", playerNumber);
-                } else {
-
-                    if (playerNumber == computerNumber + 1) {
-                        System.out.println("you guessed it!");
-                        break;
-                    } else if (playerNumber > computerNumber) {
-                        System.out.println("Too High");
-                    } else {
-                        System.out.println("Too Low");
+                for (int i = 0; i < playerInput.length(); i++) {
+                    if (playerInput.charAt(i) < 48 || playerInput.charAt(i) > 57) {
+                        isValidNumber = false;
                     }
                 }
+
+                if (isValidNumber) {
+                    int playerNumber = Integer.parseInt(playerInput);
+
+                    if (playerNumber < 1 || playerNumber > 100) {
+                        System.out.printf("Number [%d] is out of range! Try again!%n", playerNumber);
+                    } else {
+
+                        if (playerNumber == computerNumber + 1) {
+                            System.out.println("you guessed it!");
+                            System.out.println();
+                            break;
+                        } else if (playerNumber > computerNumber + 1) {
+                            System.out.println("Too High");
+                        } else {
+                            System.out.println("Too Low");
+                        }
+                    }
+                } else {
+                    System.out.printf("[%s] is not a valid number! Try again!%n", playerInput);
+                }
+            }
+
+            System.out.println("New game: press \"Y\" and after this press \"ENTER\" ");
+            System.out.println("Exit game: press \"N\" and after this press \"ENTER\" ");
+            System.out.print("Your choice is: ");
+
+            playerInput = reader.nextLine();
+
+            if (playerInput.equals("y") || playerInput.equals("Y")) {
+                System.out.println();
+                System.out.println();
+                System.out.println("NEW GAME");
+            } else if (playerInput.equals("n") || playerInput.equals("N")) {
+                break;
             } else {
-                System.out.printf("[%s] is not a valid number! Try again!%n", playerInput);
+                System.out.printf("%s is invalid command! Expected command is \"Y\" or \"N\"", playerInput);
             }
         }
+
+        System.out.println();
+        System.out.println("good bye!");
     }
 }
